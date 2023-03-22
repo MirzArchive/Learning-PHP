ef61c46770daafc65e40b8e85e0c152a090036e4
Mirza Hilmi <118604764+MirzaHilmi@users.noreply.github.com>
2023-03-23 02:01:57 +0700

    Add variable inside html tag so it can be dynamic

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
-<!-- <?php echo "<h1>Hello, world</h1>" ?> -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello, World!</title>
+</head>
+<body>
+    <h1>This is the Website Title</h1>
+    <?php 
+    $name = "Spongebob";
 
-<h1>
-    <?php echo "Hello, world" ?>
-</h1>
+    echo 'Hello, ' . $name;
+    
+    echo "Hello, $name";
+    ?>
+</body>
+</html>
