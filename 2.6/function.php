4523d913b12b7a44007463a35986246e42668c68
Mirza Hilmi <118604764+MirzaHilmi@users.noreply.github.com>
2023-03-23 14:14:30 +0700

    Finish function in php exercises

---
+++ 2.6/function.php
@@ -0,0 +1,52 @@
+<?php
+
+$title = 'Function';
+
+function output($value) {
+    echo '<pre>';
+    print_r($value);
+    echo '</pre>';
+}
+
+$foods = [
+    'Krabby Patty',
+    'Shawarma',
+    'Carbonara',
+    'Lasagna',
+    'Ramen'
+];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>PHP Fundamentals: <?= $title; ?></title>
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../assets/css/php-fundamentals.css" rel="stylesheet" />
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">PHP Fundamentals: <?= $title; ?></a>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h1 class="mt-5"></h1>
+            </div>
+        </div>
+        <div class="row">
+            <?php output($foods)?>
+        </div>
+    </div>
+</body>
+
+</html>
