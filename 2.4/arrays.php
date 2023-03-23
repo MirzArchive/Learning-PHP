3f9ce087b4219ae6306798db917e0ba42cd89ecd
Mirza Hilmi <118604764+MirzaHilmi@users.noreply.github.com>
2023-03-23 12:47:47 +0700

    Learn traditional and associative array in php

--- 2.4/arrays.php
+++ 2.4/arrays.php
@@ -1,2 +1,31 @@
 <?php
 
+// Traditional Array
+// $foods = array('Shawarma', 'Kebab', 'Fufu', 'Steak');
+
+$foods1 = ['Shawarma', 'Kebab', 'Fufu', 'Steak'];
+
+// echo $foods;
+// print_r($foods);
+
+// $index = 3;
+
+// if (isset($foods1[$index])) {
+//     echo $foods1[$index];
+// } else {
+//     echo "array foods of index $index does not exist";
+// }
+
+// Associative Array
+$foods2 = [
+    'breakfast' => 'white rice with well-done egg',
+    'lunch' => 'shawarma',
+    'dinner' => 'dry-aged steak'
+];
+
+if (isset($foods2['beverage'])) {
+    echo $foods2['beverage'];
+}
+else {
+    echo "foods item not found";
+}
