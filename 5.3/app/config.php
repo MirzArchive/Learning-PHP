c2b4d79c27200b32d1157c61f619bb0e92f4261e
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-04-05 10:39:48 +0700

    Install phpdotenv library to load existing env file

--- 5.3/app/config.php
+++ 5.3/app/config.php
@@ -1,6 +1,7 @@
 <?php
 const CONFIG = [
     'data_file' => APP_DIR . 'data.json',
+    'db' => 'mysql:dbname=glossary;host=localhost;port=3306',
     'users' => [
         'email' => 'test@test.me',
         'password' => '12345678'
