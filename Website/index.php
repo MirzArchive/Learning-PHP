495bb5a5cf958456f259af4d4327732d05104563
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-04-09 09:14:49 +0700

    Finish Chapter 3:Arithmetic

--- Website/index.php
+++ Website/index.php
@@ -1,24 +1,19 @@
 <?php
-$name = "Mirza";
 
-$user = [
-    "name" => "Mirza",
-    "email" => "fake@email.com",
-    "age" => 21,
-    "favoriteFood" => "Lasagna"
-];
+// Arithmetic operators
+// + - * / ** %
 
-$foodOrder = [
-    "name" => "Pizza",
-    "amount" => 7,
-    "pricePerPiece" => 84700.99
-];
+$x = 10;
+$y = 2;
+$z = null;
 
-echo "Hello {$user['name']}<br>";
-echo "You like {$user['favoriteFood']}<br>";
-echo "Your age is {$user['age']}<br>";
-echo "And your Email is {$user['email']}<br>";
-echo "You ordered {$foodOrder['name']}<br>";
-echo "With the amount of {$foodOrder['amount']} {$foodOrder['name']}s<br>";
-echo "The total price is {$foodOrder['pricePerPiece']} x {$foodOrder['amount']} = $" . 
-$foodOrder['pricePerPiece'] * $foodOrder['amount'];
+// $z = $x + $y;
+// $z = $x - $y;
+// $z = $x * $y;
+// $z = $x / $y;
+$z = $x ** $y;
+// $z = $x % $y;
+
+echo "The result is $z}";
+
+
