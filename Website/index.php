395ae0fc22bc0faabdd9af8f9f2e95dd3945c663
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-04-10 06:03:02 +0700

    Finish Chapter 4: and

--- Website/index.php
+++ Website/index.php
@@ -1,19 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn-PHP</title>
+</head>
+
+<body>
+    <!-- <form action="index.php" method="get">
+        <label for="username">Username:</label><br>
+        <input type="text" name="username" id="username"><br>
+        <label for="password">Password:</label><br>
+        <input type="password" name="password" id="password"><br>
+        <input type="submit" value="Login"><br>
+    </form> -->
+    <form action="index.php" method="post">
+        <label for="quantity">Quantities</label>
+        <input type="number" name="quantity" id="quantity"><br>
+        <input type="submit" value="Order"><br>
+    </form>
+</body>
+
+</html>
+
 <?php
+// echo "{$_GET['username']}<br>";
+// echo $_GET['password'];
 
-// Arithmetic operators
-// + - * / ** %
-
-$x = 10;
-$y = 2;
-$z = null;
-
-// $z = $x + $y;
-// $z = $x - $y;
-// $z = $x * $y;
-// $z = $x / $y;
-$z = $x ** $y;
-// $z = $x % $y;
-
-echo "The result is $z}";
-
-
+if (isset($_POST['quantity'])) {
+    echo "You ourder {$_POST['quantity']} x Pizzas <br>";
+    echo "The total is $" . $_POST['quantity'] * 128_500;
+}
+?>
