0b489ada25305e38823772ac548ce7fee69a60e7
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-04-10 08:08:34 +0700

    Finish Chapter 5:Math Function

--- Website/index.php
+++ Website/index.php
@@ -17,13 +17,9 @@
         <input type="submit" value="Login"><br>
     </form> -->
     <form action="index.php" method="post">
-        <label for="x">X:</label>
-        <input type="number" name="x" id="x"><br>
-        <label for="y">Y:</label>
-        <input type="number" name="y" id="y"><br>
-        <label for="z">Z:</label>
-        <input type="number" name="z" id="z"><br>
-        <input type="submit" value="Total"><br>
+        <label for="r">Radius (r):</label>
+        <input type="number" name="r" id="r"><br>
+        <input type="submit" value="Count"><br>
     </form>
 </body>
 
@@ -33,11 +29,15 @@
 // echo "{$_GET['username']}<br>";
 // echo $_GET['password'];
 
-if (isset($_POST['x']) && isset($_POST['y']) && isset($_POST['z'])) {
-    // echo abs($_POST['x']) . "<br>";
-    // echo abs($_POST['y']) . "<br>";
-    // echo abs($_POST['z']) . "<br>";
+// if (isset($_POST['x']) && isset($_POST['y']) && isset($_POST['z'])) {
+//     echo abs($_POST['x']) . "<br>";
+//     echo abs($_POST['y']) . "<br>";
+//     echo abs($_POST['z']) . "<br>";
 
-    echo rand($_POST['x'], $_POST['z']);
+//     echo rand($_POST['x'], $_POST['z']);
+// }
+
+if (isset($_POST['r'])) {
+    echo pi() * ($_POST['r'] ** 2);
 }
 ?>
