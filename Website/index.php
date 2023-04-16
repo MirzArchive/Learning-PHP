220ae5f29e88383f74f15292e9226cfaa7b0695d
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-04-16 09:35:00 +0700

    Add String methods use cases

--- Website/index.php
+++ Website/index.php
@@ -9,22 +9,32 @@
 </head>
 
 <body>
-    <form action="index.php" method="POST">
-        <input type="checkbox" name="foods[]" value="Pizza">Pizza<br>
-        <input type="checkbox" name="foods[]" value="Sate Ayam">Sate Ayam<br>
-        <input type="checkbox" name="foods[]" value="Fried Rice">Fried Rice<br>
-        <input type="checkbox" name="foods[]" value="Nasi Uduk">Nasi Uduk<br>
-        <input type="submit" value="Submit">
-    </form>
+
 </body>
+<?php
+$string = 'I am a String value';
+
+// echo strtolower($string); // to Lower Case
+// echo strtoupper($string); // to Upper Case
+// echo trim('   I have soo many space   '); // trim excess Spaces
+// echo str_pad($string, 50, '-'); // padding to right with specified char
+// echo str_replace(' ', '', $string); // Classic string replace method
+// echo strrev($string); // reverse a string value
+// echo str_shuffle($string); // Randomly shuffle String value
+// echo (strcmp($string, 'I am a String value') === 0) ? 'true' : 'false'; // compare 2 string, return 0 if true
+// echo strlen($string); // length method
+// echo strpos($string, ' '); // get the position of first occurence
+// echo substr($string, 7, 7); // substring but the third param is length of the char after the second param
+
+// $string = explode(' ', $string);
+// foreach($string as $element) {
+//     echo "{$element}<br>";
+// } // String split method
+
+// $strings = explode(' ', $string);
+// $string = implode('-', $strings);
+// echo $string; // Opposite of split / explode method
+
+?>
 
 </html>
-
-<?php
-if (isset($_POST['foods'])) {
-    $foods = $_POST['foods'];
-    foreach ($foods as $food) {
-        echo "{$food}<br>";
-    }
-}
-?>
