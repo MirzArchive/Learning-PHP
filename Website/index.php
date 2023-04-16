b2c3fce0484e0e5ea7232b7967e21d120f483ea5
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-04-16 10:02:17 +0700

    Add Post Filter sanitize

--- Website/index.php
+++ Website/index.php
@@ -9,31 +9,29 @@
 </head>
 
 <body>
-
+    <form action="" method="POST">
+        <label for="username">Username:</label><br>
+        <input type="text" name="username" id="username"><br>
+        <label for="email">Email:</label><br>
+        <input type="text" name="email" id="email"><br>
+        <label for="age">Age:</label><br>
+        <input type="text" name="age" id="age"><br>
+        <input type="submit" name="login" value="Submit"><br>
+    </form>
 </body>
 <?php
-$string = 'I am a String value';
 
-// echo strtolower($string); // to Lower Case
-// echo strtoupper($string); // to Upper Case
-// echo trim('   I have soo many space   '); // trim excess Spaces
-// echo str_pad($string, 50, '-'); // padding to right with specified char
-// echo str_replace(' ', '', $string); // Classic string replace method
-// echo strrev($string); // reverse a string value
-// echo str_shuffle($string); // Randomly shuffle String value
-// echo (strcmp($string, 'I am a String value') === 0) ? 'true' : 'false'; // compare 2 string, return 0 if true
-// echo strlen($string); // length method
-// echo strpos($string, ' '); // get the position of first occurence
-// echo substr($string, 7, 7); // substring but the third param is length of the char after the second param
+if (isset($_POST['login'])) {
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_NUMBER_INT);
 
-// $string = explode(' ', $string);
-// foreach($string as $element) {
-//     echo "{$element}<br>";
-// } // String split method
-
-// $strings = explode(' ', $string);
-// $string = implode('-', $strings);
-// echo $string; // Opposite of split / explode method
+    if (empty($email)) {
+        echo 'Email was not valid';
+    } else {
+        echo "{$username}<br>{$age}";
+    }
+}
 
 ?>
 
