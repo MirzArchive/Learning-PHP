6030084b5b75ca6beb7796ca6547d460abe140aa
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-04-18 20:38:23 +0700

    Test Case 2 TypeException Handling

--- Website/Exception.php
+++ Website/Exception.php
@@ -1,34 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <form action="" method="POST">
-        <label for="number">Number:</label><br>
-        <input type="text" name="number" id="number"><br>
+        <label for="phone-number">Phone Number:</label><br>
+        <input type="text" name="phone-number" id="phone-number">
         <input type="submit" value="Submit"><br>
     </form>
 </body>
+
 </html>
 
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (!empty($_POST['number'])) {
-        $number = $_POST['number'];
+    if (!empty($_POST['phone-number'])) {
+        $phoneNumber = $_POST['phone-number'];
 
         try {
-            echo (isEven($number)) ? 'It\'s an Even number!' : 'It\'s an Odd number!';
+            echo testPhoneNumber($phoneNumber);
         } catch (TypeError $err) {
-            echo "WARNING: {$err->getMessage()}";
+            echo "<br> WARNING! {$err->getMessage()}";
         }
     }
 }
 
-function isEven(int $n) {
-    return ($n % 2 === 0) ? true : false;
+function testPhoneNumber(int $phoneNumber)
+{
+    return "Your phone number is {$phoneNumber}";
 }
 ?>
